ace49a5dc72e791c78df6d71d1983b108d88fbbf
dasdias <das.dias6@gmail.com>
2025-01-30 18:26:59 +0100

    add installation scripts.

--- install.ps1
+++ install.ps1
@@ -1,9 +1,11 @@
 # Ensure pip is installed and upgraded
 python -m ensurepip --upgrade
+
 python -m pip install venv
+
 # create virtual environment
 python -m venv .venv
-source .venv/bin/activate
+.venv\Scripts\Activate.ps1
 # Install the 'sam' package
 python -m pip install sam
 python -m sam
